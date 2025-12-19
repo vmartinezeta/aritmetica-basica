8643f116afb2364c7ef1bff66ebf50fe93d84f83
Víctor Martínez <vito.marodriguez@gmail.com>
2025-12-19 11:58:40 -0600

    Agregando funcionalidad de Operaciones combinadas

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ const Signo = {
     MAS: 1
 }
 
-
 class Fraccion {
     constructor(signo, numerador, denominador, visibleSignoMas) {
         this.signo = signo;
         this.numerador = numerador;
         this.denominador = denominador;
-        this.visibleSignoMas = visibleSignoMas || true;
+        this.visibleSignoMas = visibleSignoMas ?? true;
     }
 
     tomarSigno() {
@@ -38,19 +37,19 @@ class Fraccion {
     simplificar() {
         this.tomarSigno();
         const maxcd = Factorizacion.maxcd(this.numerador, this.denominador);
-        return new Fraccion(this.signo, this.numerador / maxcd, this.denominador / maxcd);
+        return new Fraccion(this.signo, this.numerador / maxcd, this.denominador / maxcd, this.visibleSignoMas);
     }
 
     newInstance() {
-        return new Fraccion(this.signo, this.numerador, this.denominador);
+        return new Fraccion(this.signo, this.numerador, this.denominador, this.visibleSignoMas);
     }
 
 }
 
-
 class ExpresionAritmetica {
     constructor(...terminos) {
-        this.terminos = terminos || [];
+        this.terminos = terminos ?? [];
+        this._terminos = this.terminos;
     }
 
     addNumero(numero) {
@@ -58,13 +57,19 @@ class ExpresionAritmetica {
         return this;
     }
 
-    addFraccion(fraccion) {
-        this.terminos.push(fraccion);
+    addFraccion(signo, numerador, denominador) {
+        this.terminos.push(new Fraccion(signo, numerador, denominador));
         return this;
     }
 
-    addParentesis(signo, expresion) {
-        this.terminos.push(new ExpresionConParentesis(signo, expresion));
+    addParentesis(signo, subexpresion) {
+        this.terminos.push(new ExpresionConParentesis(signo, subexpresion));
+        this.terminos = this.terminos.map( termino => {
+            if (termino instanceof ExpresionConParentesis) {
+                return termino.reducir();
+            }
+            return termino;
+        }).filter(termino => termino !== null);
         return this;
     }
 
@@ -96,6 +101,7 @@ class ExpresionAritmetica {
             }
             fraccion.tomarSigno();
             const numerador = result.signo * result.numerador + fraccion.signo * fraccion.numerador;
+            if (!numerador) return null;
             return new Fraccion(Signo.MAS, numerador, result.denominador);
         }, null);
     }
@@ -113,7 +119,7 @@ class ExpresionAritmetica {
     }
 
     toString() {
-        return this.terminos.reduce((text, termino)=> {
+        return this._terminos.reduce((text, termino) => {
             if (!text) {
                 termino.visibleSignoMas = false;
                 return termino.toString();
@@ -122,7 +128,6 @@ class ExpresionAritmetica {
         }, null);
     }
 }
-
 
 class ExpresionConParentesis {
     constructor(signo, expresion, visibleSignoMas) {
@@ -133,20 +138,51 @@ class ExpresionConParentesis {
 
     reducir() {
         const fr = this.expresion.reducir();
+        if (!fr) return null;
         fr.tomarSigno();
         return new Fraccion(fr.signo*this.signo, fr.numerador, fr.denominador);
     }
 
     toString() {
         if (this.signo === Signo.MAS && this.visibleSignoMas) {
-            return `+ ${this.expresion.toString()}`;
+            return `+ (${this.expresion.toString()})`;
         } else if (this.signo === Signo.MAS) {
-            return this.expresion.toString();
+            return `(${this.expresion.toString()})`;
         }
-        return `- ${this.expresion.toString()}`;
+        return `- (${this.expresion.toString()})`;
     }
 }
 
+class Multiplicacion {
+    constructor(...terminos) {
+        this.terminos = terminos.map(t => {
+            if (t instanceof Fraccion) {
+                return t;
+            }
+            return new Fraccion(Signo.MAS, t, 1);
+        });
+    }
+
+    multiplicar() {
+        return this.terminos.reduce((producto, fraccion)=> {
+            if (!producto) {
+                fraccion.tomarSigno();
+                return fraccion;
+            }
+            fraccion.tomarSigno();
+            const signo = producto.signo * fraccion.signo;
+            return new Fraccion(signo, producto.numerador*fraccion.numerador, producto.denominador*fraccion.denominador);
+        }, null);
+    }
+
+    toString() {
+        return this.terminos.reduce((result, termino)=> {
+            if (!result) return termino.toString();
+            return result.toString() + ' * ' + termino.toString();
+        }, '');
+    }
+
+}
 
 class Factorizacion {
     static maxcd(a, b) {
@@ -194,27 +230,30 @@ class Factorizacion {
     }
 }
 
-
 class Calculadora {
     constructor(expresion) {
         this.expresion = expresion;
     }
 
     resolver() {
-        return this.expresion.reducir();
+        const fraccion = this.expresion.reducir();
+        fraccion.visibleSignoMas = false;
+        const nuevaFraccion = fraccion.simplificar();
+        console.log(expresion.toString(), ' = ', fraccion.toString(), ' = ', fraccion.toString()!== nuevaFraccion.toString() ? nuevaFraccion.toString():'');
     }
 
 }
 
-
 const expresion = new ExpresionAritmetica();
+
+const subexpresion = new ExpresionAritmetica();
+subexpresion.addFraccion(Signo.MAS, 1, 4)
+            .addFraccion(Signo.MAS, 3, 10);
+
 expresion.addNumero(2)
-    .addFraccion(new Fraccion(Signo.MENOS, 7, 3))
-    .addFraccion(new Fraccion(Signo.MAS, 5, 6));
+    .addFraccion(Signo.MENOS, 7, 3)
+    .addFraccion(Signo.MAS, 5, 6)
+    .addParentesis(Signo.MAS, subexpresion);
 
 const calculadora = new Calculadora(expresion);
-const fraccion = calculadora.resolver();
-fraccion.visibleSignoMas = false;
-const nuevaFraccion = fraccion.simplificar();
-nuevaFraccion.visibleSignoMas = false;
-console.log(expresion.toString(), ' = ', fraccion.toString(), ' = ', fraccion.toString()!== nuevaFraccion.toString()? nuevaFraccion.toString():'');
+calculadora.resolver();
